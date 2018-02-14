271593cea3949a4c35d690eeb12cb7762843137f
Joshua H. Wang <wangjoshuah@gmail.com>
2018-02-13 22:22:44 -0800

    solve using stdin

--- day-08/solution.py
+++ day-08/solution.py
@@ -1,15 +1,17 @@
-n = int(input().strip())
+import sys
+
+n = int(sys.stdin.readline().strip())
 phoneBook = dict()
 
 for i in range(0, n):
-    entry = input().strip().split(' ')
+    entry = sys.stdin.readline().strip().split(' ')
     phoneBook[entry[0]] = entry[1]
 
-query = input().strip()
+query = sys.stdin.readline().strip()
 while query:
     phoneNumber = phoneBook.get(query)
     if phoneNumber:
         print(query + '=' + phoneNumber)
     else:
         print('Not found')
-    query = input().strip()
+    query = sys.stdin.readline().strip()
