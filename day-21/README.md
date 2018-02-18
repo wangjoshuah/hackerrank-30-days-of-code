2d61e63c52f9dc5c65a6c9e115090cecf3fadd9e
Joshua H. Wang <wangjoshuah@gmail.com>
2018-02-18 09:13:52 -0800

    solve day 21

---
+++ day-21/README.md
@@ -0,0 +1,26 @@
+# Day 21: Generics
+
+### Objective
+
+Today we're discussing Generics; be aware that not all languages support this construct.
+Check out the [Tutorial](https://www.hackerrank.com/challenges/30-generics/tutorial)
+tab for learning materials and an instructional video!
+
+### Task
+
+Write a single generic function named _printArray_;
+this function must take an array of generic elements as a parameter.
+The locked Solution class in your editor tests your function.
+
+### Input Format
+
+The _Solution_ class will pass different types of arrays to your _printArray_ function.
+
+### Constraints
+
+* You must have exactly 1 function named _printArray_.
+
+### OutputFormat
+
+Your _printArray_ ffunctionshould print each element of its generic array parameter on a new line.
+
---
+++ day-21/Solution.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+class Printer <T> {
+    public void printArray(T[] elements) {
+        for (T element : elements) {
+            System.out.println(element);
+        }
+    }
+}
+
+public class Solution {
+
+    public static void main(String args[]){
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        Integer[] intArray = new Integer[n];
+        for (int i = 0; i < n; i++) {
+            intArray[i] = scanner.nextInt();
+        }
+
+        n = scanner.nextInt();
+        String[] stringArray = new String[n];
+        for (int i = 0; i < n; i++) {
+            stringArray[i] = scanner.next();
+        }
+
+        Printer<Integer> intPrinter = new Printer<Integer>();
+        Printer<String> stringPrinter = new Printer<String>();
+        intPrinter.printArray( intArray  );
+        stringPrinter.printArray( stringArray );
+        if(Printer.class.getDeclaredMethods().length > 1){
+            System.out.println("The Printer class should only have 1 method named printArray.");
+        }
+    }
+}
