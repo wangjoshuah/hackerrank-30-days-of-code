e70e597a084adddf8253b2fd89dd945506814593
Joshua H. Wang <wangjoshuah@gmail.com>
2018-02-26 22:52:27 -0800

    solve day 7 in the spirit of arrays

--- day-07/solution.py
+++ day-07/solution.py
@@ -1,5 +1,14 @@
 n = int(input().strip())
 arr = [int(arr_temp) for arr_temp in input().strip().split(' ')]
-arr.reverse()
 
-print(' '.join(str(i) for i in arr))
+reversed_array = []
+for i in range(len(arr)):
+    reversed_array.append(arr[n-i-1])
+
+# print(' '.join(str(i) for i in reversed_array))
+
+output_string = ''
+for i in range(len(reversed_array)):
+    output_string += str(reversed_array[i]) + ' '
+
+print(output_string)
