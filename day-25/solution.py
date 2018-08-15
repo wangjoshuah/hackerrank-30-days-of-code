196ad1a6d8eb5c0063e35da0df65e53f4a7db419
Joshua H. Wang <wangjoshuah@gmail.com>
2018-08-14 23:13:11 -0700

    add python solution for day 25

---
+++ day-25/solution.py
@@ -0,0 +1,21 @@
+import math
+
+def isPrime(n):
+    if n <= 1:
+        return False
+    sqrt_n = math.sqrt(n)
+    if sqrt_n.is_integer():
+        return False
+    for i in range(2, int(sqrt_n)+1):
+        if n%i == 0:
+            return False
+    return True
+
+
+num_test_cases = int(input())
+for i in range(num_test_cases):
+    n = int(input())
+    if isPrime(n):
+        print('Prime')
+    else:
+        print('Not prime')
